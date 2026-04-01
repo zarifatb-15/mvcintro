8a2b196bf11c7af7cb370df69643174de84c269d
zarifatb-15 <zarifatb@code.edu.az>
2026-04-01 06:45:48 +0400

    viewstart

--- WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ec00c483f56ebbda6f92a195739267dc3e6efc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c410ef6c41a810b9ae0f6f0d23476e9138dd77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
